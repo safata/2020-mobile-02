9c14d1c95cd3e7f169d6e4d469ed52d9a3437bbb
safata <annisasafataa@gmail.com>
2020-02-15 18:35:59 +0700

    Konversi Layout 2

---
+++ app/src/main/java/id/putraprima/marketplacelayout/HalamanRegister.java
@@ -0,0 +1,14 @@
+package id.putraprima.marketplacelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalamanRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_register);
+    }
+}
